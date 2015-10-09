2d3b5fa1dba1b8eb8ee6849638f3f2dbe4d9f53c
Ovyn Flavian <ovyn.flavian@gmail.com>
2015-10-09 11:40:39 +0200

    Premier test css avec bootstrap sur le ficher index.php

--- Projet/Web/index.php
+++ Projet/Web/index.php
@@ -23,28 +23,52 @@ $configIni = parse_ini_file("config.ini", true);
 </head>
 <body>
 <header>
-    <?php
-        if(!$isConnect)include(".". MENU_ANONYME);
-        else include(".". MENU_CONNECTER);
-    ?>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">EveryDayIdea</a>
+            </div>
+            <div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="./">Home</a></li>
+                    <?php if(!$isConnect){?>
+                        <li><a href="./Page/connexion.page.php">Connexion</a></li>
+                        <li><a href="./Page/inscription.page.php">Inscription</a></li>
+                    <?php }else{ ?>
+                        <li><a href="./Page/connexion.page.php">Déconnexion</a></li>
+                        <li><a href="./Page/profil.page.php">Profil</a></li>
+                    <?php }?>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </header>
-<section id="section_corps">
-    <div id="div_left">
-        &nbsp;
-    </div>
-    <div id="div_center">
-        <h1>Page d'accueil</h1>
-        <?php
-        if(!isConnect())
-            echo "<p style='text-align: center'>Il n'y a pas de user de connecté</p>";
-        else
-            echo "<p style='text-align: center'>Le user connecté est : </p><table>". getSessionUser()->toStringList(). "</table>" ?>
-    </div>
-    <div id="div_right">
-        &nbsp;
-    </div>
+<section class="container">
+    <section class="jumbotron">
+        <h1>Accueil Page</h1>
+        <p>
+            <?php if($isConnect){ ?>
+                Bienvenu(e) <?php echo getSessionUser()->getUserName() ?> sur le site de EveryDayIdea
+            <?php }else{?>
+                Bienvenu(e) sur le site de EveryDayIdea
+            <?php }?>
+        </p>
+    </section>
+    <section class="row">
+        <article class="col-sm-6">
+            <h3>Activité du jours</h3>
+            <?php foreach($configIni['SERVEUR_ADDRESS'] as $key => $value){ ?>
+                <p><?php echo $key. "->". $value; ?></p>
+            <?php }?>
+        </article>
+        <article class="col-sm-6">
+            <h3>Activité de groupe du jours</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+        </article>
+    </section>
 </section>
-<footer>
+<footer class="panel-footer">
     &copy; everydayidea.com. Contactez <a href="mailto:<?php echo $configIni['ADMINISTRATEUR']['mail']?>">l'administrateur</a>
 </footer>
 
