3eb094790e7a3f8bf18e7297b8d2c5da3bd729b9
ArnaudWillame <naunau21@hotmail.com>
2015-10-14 01:34:54 +0200

    Login
    -Login d'un utilisateur(sécurité basique pour le moment)
    -ajout d'un bouton "register" qui mène a une autre activité ( pour le
    moment il n'y a que le layout, rien derriere)

---
+++ Projet/Android/IntegrationProjetV0/app/src/main/java/com/example/arnaud/integrationprojetv0/LogIn.java
@@ -0,0 +1,14 @@
+package com.example.arnaud.integrationprojetv0;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LogIn extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+
+    }
+}
---
+++ Projet/Android/IntegrationProjetV0/app/src/main/java/com/example/arnaud/integrationprojetv0/Register.java
@@ -0,0 +1,19 @@
+package com.example.arnaud.integrationprojetv0;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nauna on 14-10-15.
+ */
+public class Register extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+
+    }
+}
+
+
+
