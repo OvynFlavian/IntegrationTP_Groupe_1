d170d872784fec20387efebb369e7d9b477b6a9f
Ovyn Flavian <ovyn.flavian@gmail.com>
2015-10-18 15:22:10 +0200

    afficheCategorie.lib

--- Projet/Web/Library/Page/choisirCategorie.lib.php
+++ Projet/Web/Library/Page/choisirCategorie.lib.php
@@ -10,10 +10,19 @@
 function afficherCategorie() {
 
       $cm = new CategorieManager(connexionDb());
+            $i = 0;
             $tab = $cm->getAllCategorie();
             foreach ($tab as $elem) {
+                $i++;
                 $cat = $elem->getLibelle();
-                echo "<article class='col-sm-6'>";
+                if($i%2 != 0)
+                {
+                    echo "<article class='col-sm-6' style='text-align: right'>";
+                }
+                else
+                {
+                    echo "<article class='col-sm-6' style='text-align: left'>";
+                }
                 echo "<a href='proposerActivite.page.php?categorie=$cat' title='$cat'><img src='../Images/$cat.jpg' alt='$cat'/></a>";
                 echo "</article>";
             }
