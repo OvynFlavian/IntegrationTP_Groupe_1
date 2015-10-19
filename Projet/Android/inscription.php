a6b5d292978bae327f79bca58ad51aca76abc6c4
ArnaudWillame <naunau21@hotmail.com>
2015-10-19 23:49:46 +0200

    ajout sécurité Inscription
    Ajout de vérification sur les champs inscrit, de l'envoi d'un email a
    l'inscription, ajout a la table 'activation' de la BDD, ajout de
    fonctions utiles.

--- Projet/Android/inscription.php
+++ Projet/Android/inscription.php
@@ -48,12 +48,13 @@ $con = mysqli_connect(HOST,USER,PASS,DB);
                     $return .= "Cette adresse mail est déjà utilisée, veuillez en choisir une autre ! <br>";
                 if(!$validUserName)
                     $return .= "Ce login est déjà pris, veuillez en choisir en autre ! <br>";
-				/*
+				
                 if(!champsEmailValable($email))
                 {
                     $return .= "Votre adresse mail contient des caractères indésirables !";
                     $champValid = false;
                 }
+				
                 if (!champsLoginValable($userName))
                 {
                     $return .= "Votre nom d'utilisateur contient des caractères indésirables !";
@@ -64,21 +65,51 @@ $con = mysqli_connect(HOST,USER,PASS,DB);
                     $return .= "Votre mot de passe contient des caractères indésirables !";
                     $champValid = false;
                 }
-                if($validUserMail and $validUserName and $champValid)
-                  */ 
+                if($validUserMail and $validUserName and $champValid and ($return== "true")){
+                  
 			  
-				if($return== "true"){
+					//if($return== "true"){
 			  
 					$sql="INSERT INTO user(userName, password, email) VALUES('".$userName."', '".$mdp."', '".$email."')";
 					mysqli_query ($con,$sql);
 				}
+				
 				
             }
 			echo $return;
 			
         }
 		
-		function nbUserByUsername($userName){	
+		
+		
+		$query = mysqli_query($con,"select * from user where userName='".$userName."'");
+		$row = mysqli_fetch_assoc($query);
+		$userId = $row['id'];
+		$userNameBdd = $row['userName'];
+		$passwordBdd= $row['password'];
+		$emailBdd= $row['email'];
+		
+
+		
+
+        $code_aleatoire = genererCode();
+        $adresseAdmin = "andrewblake@hotmail.fr";
+        $to = $email;
+        $sujet = "Confirmation de l'inscription";
+        $entete = "From:" . $adresseAdmin . "\r\n";
+        $message = "Nous confirmons que vous êtes officiellement inscrit sur le site EveryDayIdea <br>
+									Votre login est : " . $userNameBdd . " <br>   
+									Votre email est : " . $emailBdd . " <br>
+									Votre lien d'activation est : <a href='www.everydayidea/activation.php?code=" . $code_aleatoire . "'>www.everydayidea/activation.php?code=" . $code_aleatoire . "</a>";
+       // mail($to, $sujet, $message, $entete);
+      //  echo "Votre inscription est dorénavant complète ! Un email vous a été envoyé avec vos informations et votre code d'activation !";
+		
+				$sql="INSERT INTO activation (id_user, code, date) VALUES('".$userId."', '".$code_aleatoire."', NOW())";
+		mysqli_query ($con,$sql);
+
+
+
+function nbUserByUsername($userName){	
 			$con = mysqli_connect(HOST,USER,PASS,DB);
 			$query="SELECT * FROM user WHERE userName ='".$userName."' ";
 			if($result=mysqli_query($con,$query)){
@@ -98,54 +129,42 @@ $con = mysqli_connect(HOST,USER,PASS,DB);
 			}
 		}
 		
-		/*
-		if ($return=="true"){
-			
-			$sql="INSERT INTO user(userName, password, email) VALUES('".$userName."', '".$mdp."', '".$email."')";
-			mysqli_query ($con,$sql)
-			$return="true";				
-			
+		function champsEmailValable($champ) {
+			if(preg_match('#^[a-zA-Z0-9@._]*$#', $champ)) {
+				return true;
+			}
+			else {
+				return false;
+			}
 		}
-		//else $return="false";
-       */
 		
-		
+		function champsLoginValable($champ) {
+			if(preg_match('#^[a-zA-Z0-9 \ éàèîêâô! ]*$#', $champ)) {
+				return true;
+			}
+			else {
+			return false;
+			}
+		}
 
-      /*  $code_aleatoire = genererCode();
-        $adresseAdmin = "andrewblake@hotmail.fr";
-        $to = $userToAdd->getEmail();
-        $sujet = "Confirmation de l'inscription";
-        $entete = "From:" . $adresseAdmin . "\r\n";
-        $message = "Nous confirmons que vous êtes officiellement inscrit sur le site EveryDayIdea <br>
-									Votre login est : " . $userToAdd->getUserName() . " <br>
-									Votre email est : " . $userToAdd->getEmail() . " <br>
-									Votre lien d'activation est : <a href='www.everydayidea/activation.php?code=" . $code_aleatoire . "'>www.everydayidea/activation.php?code=" . $code_aleatoire . "</a>";
-        mail($to, $sujet, $message, $entete);
-        echo "Votre inscription est dorénavant complète ! Un email vous a été envoyé avec vos informations et votre code d'activation !";
-        /** @var $um : un nouvel user qui va être ajouté à la BDD
-        J'ajoute le nouvel user à la BDD*/
+		function champsMdpValable($champ) {
+			if(preg_match('#^[a-zA-Z0-9 \ éàèîêâô!? ]*$#', $champ)) {
+           return true;
+			}
+			else {
+			return false;
+			}
+		}
 		
-       /* $um = new UserManager(connexionDb());
-        $um->addUser($userToAdd);
-        /**
-         * Ici j'ai besoin de savoir quel est le user id du nouveau membre ajouté pour pouvoir le mettre dans l'ajout du code d'activation de cet user
-         * Donc je vais le rechercher en base de donnée où il vient d'être ajouté
-         */
-       
-/*	   $user = $um->getUserByUserName($userToAdd->getUserName());
+		function genererCode() {
+			$characts    = 'abcdefghijklmnopqrstuvwxyz';
+			$characts   .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+			$characts   .= '1234567890';
+			$code_aleatoire      = '';
 
-        $userid = $user->getId();
-        /**
-         * J'ajoute le nouveau code d'activation à la BDD
-         */
-        
-		
-		/*$am = new ActivationManager(connexionDb());
-        $activation = new Activation(array(
-            "code" => $code_aleatoire,
-            "id_user" => $userid,
-            "libelle" => "Inscription",
-            ));
-        $am->addActivation($activation);
-*/
+			for($i=0;$i < 6;$i++){
+				$code_aleatoire .= substr($characts,rand()%(strlen($characts)),1);
+			}
+			return $code_aleatoire;
+		}
    ?>
