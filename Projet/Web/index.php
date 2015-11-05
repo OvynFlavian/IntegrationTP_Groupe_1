628f8a3bbb0c3f3351e19c901fb3d7e22874d44c
PletinckxJ <julien.pletinckx@hotmail.com>
2015-11-05 23:41:47 +0100

    Changement index

--- Projet/Web/index.php
+++ Projet/Web/index.php
@@ -35,6 +35,7 @@ $configIni = getConfigFile();
 <header>
     <?php include("./Menu/menuGeneral.lib.php"); ?>
 </header>
+
 <section class="container">
     <section class="jumbotron">
         <h1>Page d'accueil</h1>
@@ -61,10 +62,10 @@ $configIni = getConfigFile();
                            echo "Vous n'avez pas encore choisi d'activité aujourd'hui ! <a href='Page/choisirCategorie.page.php'>Choississez-en une</a> !";
                        } else {
                            $act = $am->getActivityById($tab[0]['id_activity']);
-                           echo "Votre activité choisie du jour est <h3>".$act->getLibelle()."</h3>";
-                           echo "<br> Sa description est <h4>".$act->getDescription()."</h4>";
-                           echo "<br> Il est toujours possible de la changer via <b><a href='Page/choisirCategorie.page.php'>le choix d'activités</a></b> !";
-                           echo "<br> Bon amusement !";
+                           echo "<p>Votre activité choisie du jour est <h3>".$act->getLibelle()."</h3></p>";
+                           echo "<p> Sa description est <h4>".$act->getDescription()."</h4></p>";
+                           echo "<p> Il est toujours possible de la changer via <b><a href='Page/choisirCategorie.page.php'>le choix d'activités</a></b> !</p>";
+                           echo "<p><b> Bon amusement !</b></p>";
                        }
 
                     }
@@ -74,12 +75,26 @@ $configIni = getConfigFile();
 
         </article>
         <article class="col-sm-6">
-            <h3>Qu'est-ce que EveryDayIdea ?</h3>
-            <p>Vous vous ennuyez et vous ne savez absolument pas quoi faire ? <b>Nous avons la solution</b> pour vous !</p>
-            <p>EveryDayIdea vous permet de <a href="Page/choisirCategorie.page.php"><b>trouver une activité selon une catégorie choisie !</b></a></p>
-            <p>Si elle vous plait, acceptez-la, sinon vous pouvez continuer à fouiller jusqu'à trouver celle qui vous plait !</p>
-            <p>Si vous avez envie de participer à la vie du site, <a href="Page/inscription.page.php"><b>inscrivez-vous !</b></a></p>
-            <p>Vous pourrez ainsi proposer vos propres idées et permettre à d'autres personnes d'y participer !</p>
+            <?php
+            if (!isConnect()) {
+                echo "<h3>Qu'est-ce que EveryDayIdea ?</h3>";
+                echo "<p>Vous vous ennuyez et vous ne savez absolument pas quoi faire ? <b>Nous avons la solution</b> pour vous !</p>";
+                echo "<p>EveryDayIdea vous permet de <a href='Page/choisirCategorie.page.php'><b>trouver une activité selon une catégorie choisie !</b></a></p>";
+                echo "<p>Si elle vous plait, acceptez-la, sinon vous pouvez continuer à fouiller jusqu'à trouver celle qui vous plait !</p>";
+                echo "<p>Si vous avez envie de participer à la vie du site, <a href='Page/inscription.page.php'><b>inscrivez-vous !</b></a></p>";
+                echo "<p>Vous pourrez ainsi proposer vos propres idées et permettre à d'autres personnes d'y participer !</p>";
+            } else {
+                echo "<h3>Bienvenue sur EveryDayIdea ! </h3>";
+                echo "<p>Vous vous ennuyez et vous ne savez absolument pas quoi faire ? <b>Nous avons la solution</b> pour vous !</p>";
+                echo "<p>EveryDayIdea vous permet de <a href='Page/choisirCategorie.page.php'><b>trouver une activité selon une catégorie choisie !</b></a></p>";
+                echo "<p>Si elle vous plait, acceptez-la, sinon vous pouvez continuer à fouiller jusqu'à trouver celle qui vous plait !</p>";
+                echo "<p> Maintenant que vous êtes connecté, vous avez droit à des tonnes de fonctionnalités supplémentaires !";
+                echo "<p> Dorénavant, vous pouvez <a href='Page/listeMembres.page.php'><b> Chercher des amis !</b></a> ou bien <a href='Page/amis.page.php'><b>Gérer vos amis et vos demandes d'amis !</b> </a></p>";
+                echo "<p> N'hésitez pas non plus à <a href='Page/ajouterActivite.page.php'><b>ajouter une activité </b></a> si le coeur vous en dit !</p>";
+                echo "<p> Mais si vous voulez ajouter plus d'une activité par semaine ou pouvoir vous grouper avec d'autres personnes pour faire une activité :";
+                echo "<p align='center'><b>Devenez Premium grâce à notre bouton Don dans la barre de navigation ! </b></p>";
+            }
+            ?>
         </article>
     </section>
     <?php if(isConnect()){?>
@@ -106,9 +121,16 @@ $configIni = getConfigFile();
     </section>
     <?php }?>
 </section>
-<footer class="footer panel-footer navbar-fixed-bottom">
-    &copy; everydayidea.com. Contactez <a href="mailto:<?php echo $configIni['ADMINISTRATEUR']['mail']?>">l'administrateur</a>
-</footer>
+
+<div class="footer-container">
+    <div class="row">
+    <footer class="footer panel-footer navbar-fixed-bottom">
+     &copy; everydayidea.com. Contactez <a href="mailto:<?php echo $configIni['ADMINISTRATEUR']['mail']?>">l'administrateur</a>
+    </footer>
+    </div>
+</div>
+
+
 
 </body>
 </html>
