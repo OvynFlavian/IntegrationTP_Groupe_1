d04a188195d4ecda0ab3b64cab160e3c48a75cf3
PletinckxJ <julien.pletinckx@hotmail.com>
2015-11-10 21:37:23 +0100

    changement index

--- Projet/Web/Form/connexion.form.php
+++ Projet/Web/Form/connexion.form.php
@@ -15,9 +15,9 @@
         </div>
     </div>
     <div class="form-group">
-        <label class="control-label col-sm-2" for="mdp">Password:</label>
+        <label class="control-label col-sm-2" for="mdp">Mot de passe:</label>
         <div class="col-sm-10">
-            <input type="password" class="form-control" id="mdp" name="mdp" placeholder="Enter password" required>
+            <input type="password" class="form-control" id="mdp" name="mdp" placeholder="Entrez votre mot de passe" required>
         </div>
     </div>
     <div class="form-group">
